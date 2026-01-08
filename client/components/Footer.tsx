2f5bf74a5f57211b03d6450cd800f56649e27393
Habeebamoo <habeebamoo08@gmail.com>
2026-01-08 15:28:35 +0100

    updated footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -5,33 +5,18 @@ const Footer = () => {
     redirect("https://habeebamoo.netlify.app")
   }
 
+  const year = new Date().getFullYear()
+
   return (
-    <footer className="bg-indigo-950 p-8 pb-20 text-white mt-25">
+    <footer className="bg-indigo-950 p-8 pb-10 text-white mt-25">
       <div>
         <h1 className="font-jsans text-lg">UCEESTAR</h1>
         <p className="text-[12px] font-jsans-light mt-1">By PJ Star LTD.</p>
       </div>
 
-      <div className="mt-15">
-        <h1 className="font-jsans text-lg">Contact Us</h1>
-        <p className="underline font-jsans-light mt-4 text-sm">uceestar@gmail.com</p>
-        <p className="text-sm font-jsans-light mt-4">Mobile: +234 905 7312 712</p>
-        <p className="text-sm font-jsans-light mt-4">Whatsapp: +234 902 4228 180</p>
-      </div>
+      <hr className="text-gray-700 mt-8" />
 
-      <p className="mt-15 font-jsans-light text-sm">Discover curated collections of premium footwears, cutting-edge technologies and lifestyle essentials crafted for the modern world</p>
-
-      <div className="mt-15">
-        <h1 className="font-jsans text-lg">Quick Links</h1>
-        <p className="font-jsans-light mt-4 text-sm">Search</p>
-        <p 
-          onClick={toDev} 
-          className="cursor-pointer font-jsans-light mt-4 text-sm"
-        >
-          Developers Profile
-        </p>
-        <p className="font-jsans-light mt-4 text-sm">FAQ</p>
-      </div>
+      <p className="font-jsans-light text-gray-300 text-sm mt-8">Uceestar {year}. All right reserved</p>
     </footer>
   )
 }
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = ({ setNavbarActive }: Props) => {
 
   return (
     <div className="z-[9999] fixed isolate inset-0 top-0 bottom-0 left-0 right-0 bg-black/70">
-      <div className="fixed top-0 bottom-0 right-0 w-[100%] sm:w-[250px] lg:w-[300px] bg-white">
+      <div className="fixed top-0 bottom-0 right-0 w-[60%] sm:w-[250px] lg:w-[300px] bg-white">
         {/* cancel btn */}
         <div 
           onClick={close}
