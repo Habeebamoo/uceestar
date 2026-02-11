e28e3e3f5a8af95fac2a9d40b301c6b6a7cad9c8
Habeebamoo <habeebamoo08@gmail.com>
2026-02-11 13:43:08 +0100

    updated footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -11,34 +11,40 @@ const Footer = () => {
 
   return (
     <footer className="bg-indigo-950 p-8 pb-10 text-white mt-25">
-      <div>
-        <div className="h-10 w-10 bg-white flex-center rounded-full">
-          <img src="/logo.png" className="h-5" />
-        </div>
-        <h1 className="font-jsans mt-3 text-lg">UCEESTAR</h1>
-        <p className="text-[12px] text-gray-400 font-jsans-light mt-2">By PJ Star LTD.</p>
-      </div>
-
-      <div className="mt-10">
-        <div className="flex-start gap-2 font-jsans-light">
-          <Mail className="text-gray-500" size={16} />
-          <p className="text-gray-400 text-sm">uceestar@gmail.com</p>
+      <section className="md:grid md:grid-cols-3">
+        <div>
+          <div className="h-10 w-10 bg-white flex-center rounded-full">
+            <img src="/logo.png" className="h-5" />
+          </div>
+          <h1 className="font-jsans mt-3 text-lg">UCEESTAR</h1>
+          <p className="text-[12px] text-gray-300 font-jsans-light mt-2">By PJ Star LTD.</p>
         </div>
 
-        <div className="flex-start gap-2 font-jsans-light mt-2">
-          <Phone className="text-gray-500" size={16} />
-          <p className="text-gray-400 text-sm">+243 905 731 2712</p>
-        </div>
+        <div className="max-md:mt-10 md:flex-center md:flex-col">
+          <div>
+            <div className="flex-start gap-2 font-jsans-light">
+              <Mail className="text-gray-400" size={16} />
+              <p className="text-gray-300 text-sm">uceestar@gmail.com</p>
+            </div>
 
-        <div className="flex-start gap-2 font-jsans-light mt-2">
-          <BsWhatsapp className="text-gray-500" size={16} />
-          <p className="text-gray-400 text-sm">+243 902 422 8180</p>
+            <div className="flex-start gap-2 font-jsans-light mt-2">
+              <Phone className="text-gray-400" size={16} />
+              <p className="text-gray-300 text-sm">+243 905 731 2712</p>
+            </div>
+
+            <div className="flex-start gap-2 font-jsans-light mt-2">
+              <BsWhatsapp className="text-gray-400" size={16} />
+              <p className="text-gray-300 text-sm">+243 902 422 8180</p>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
 
       <hr className="text-gray-700 mt-8" />
 
-      <p className="font-jsans-light text-gray-400 text-[12px] mt-8">Uceestar &copy; {year}. All right reserved</p>
+      <p className="font-jsans-light text-gray-300 text-[12px] mt-8">
+        Uceestar &copy; {year}. All right reserved
+      </p>
     </footer>
   )
 }
